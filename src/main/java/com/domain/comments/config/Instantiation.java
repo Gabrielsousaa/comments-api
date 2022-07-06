039e9ca61a9daa28619e5fbe7f3877fc949a3a42
Gabrielsousaa <gsousax2008@hotmail.com>
2022-07-06 17:12:34 -0300

    Recursos da entidade Post criados

--- src/main/java/com/domain/comments/config/Instantiation.java
+++ src/main/java/com/domain/comments/config/Instantiation.java
@@ -1,22 +1,33 @@
 package com.domain.comments.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.domain.comments.domain.Post;
 import com.domain.comments.domain.User;
+import com.domain.comments.repositories.PostRepository;
 import com.domain.comments.repositories.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
   @Autowired
+  private PostRepository postRepository;
+
+  @Autowired
   private UserRepository userRepository;
 
   @Override
   public void run(String... args) throws Exception {
+
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
     userRepository.deleteAll();
 
     User maria = new User(null, "Maria Brown", "maria@gmail.com");
@@ -24,6 +35,10 @@ public class Instantiation implements CommandLineRunner {
     User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
     userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+    Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para SP", maria);
+    Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", maria);
+    postRepository.saveAll(Arrays.asList(post1, post2));
 
   }
 
