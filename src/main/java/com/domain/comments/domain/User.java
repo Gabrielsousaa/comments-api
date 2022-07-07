4e85b53b684df8077ca074a7c1091c8dab564811
Gabrielsousaa <gsousax2008@hotmail.com>
2022-07-07 09:38:39 -0300

    Referenciando os posts do usuario

--- src/main/java/com/domain/comments/domain/User.java
+++ src/main/java/com/domain/comments/domain/User.java
@@ -1,7 +1,12 @@
 package com.domain.comments.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Id;
+
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
@@ -9,9 +14,13 @@ public class User implements Serializable {
 
   private static final long serialVersionUID = 1L;
 
+  @Id
   private String id;
   private String name;
   private String email;
+
+  @DBRef(lazy = true)
+  private List<Post> posts = new ArrayList<>();
 
   public User() {
   }
@@ -44,6 +53,14 @@ public class User implements Serializable {
 
   public void setEmail(String email) {
     this.email = email;
+  }
+
+  public List<Post> getPosts() {
+    return posts;
+  }
+
+  public void setPosts(List<Post> posts) {
+    this.posts = posts;
   }
 
   @Override
